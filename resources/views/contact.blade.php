f9942824c06668afef744c09645383fc2286bda6
Unikornis <szikisanyi@gmail.com>
2024-12-05 06:10:11 +0100

    Több, módosított és új fájl->web.php ->contact és tasklist

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Kapcsolat</>
+<h2>Üdvözlet {{ $name }}!</h2>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    // return ["kulcs"=>"érték"];
+    $username = 'John';
+return view('contact', [
+'name' => $username
+]);
+});
+
+// Route::view('/contact', 'contact');
+
+Route::get('/tomb', function () {
+    $tasks = [
+    'Go to the store',
+    'Go to the market',
+    'Go to the work',
+    'Task #4'
+    ];
+/*
+    return view('tasklist', [
+    'tasks' => $tasks
+    ]);
+     */
+    return view('tasklist')->withTasks($tasks);
+    });
